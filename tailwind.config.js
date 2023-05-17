51bfd8e5a11f2667f35fdd8982cb0a0d491699d6
Ivan Pyslar <Ivanpyclar@gmail.com>
2023-05-17 18:26:35 +0200

    Add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    screens: {
+      xs: '600px',
+      sm: '768px',
+      md: '991px',
+      lg: '1400px',
+    },
+    extend: {
+      width: {
+        100: '100%',
+      },
+    },
+  },
+  plugins: [],
+}
+
